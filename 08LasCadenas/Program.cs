ded57f73972d8024b3f739202acb1d4b55bdc73d
G58 <freddy.fazbear.troll@gmail.com>
2025-01-14 13:49:11 -0600

    Miracle init

---
+++ 08LasCadenas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08LasCadenas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
