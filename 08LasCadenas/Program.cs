be1857e57bbaf39d8630c2ace3de45632cd75239
G58 <freddy.fazbear.troll@gmail.com>
2025-01-16 00:11:50 -0600

    La clase StringBuilder y Método Append

--- 08LasCadenas/Program.cs
+++ 08LasCadenas/Program.cs
@@ -1,32 +1,20 @@
-﻿namespace _08LasCadenas
+﻿using System.Text;
+
+namespace _08LasCadenas
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Replace
-            string text = "hola mundo";
+            StringBuilder numeros = new StringBuilder();
+            for (int i = 1; i <= 100; i++)
+            {
+                numeros.Append(i);
+                numeros.Append(", ");
+            }
 
-            // Reemplazamos "hola" por "Hola" en una nueva cadena
-            string newText = text.Replace("hola", "Hola");
-            Console.WriteLine(text);
-            Console.WriteLine(newText);
-
-
-            // Trim
-            string text2 = "---Hola Mundo---";
-
-            string trimText2 = text2.Trim('-');
-            Console.WriteLine($"Sin con Trim: {text2}");
-            Console.WriteLine($"Texto con Trim: {trimText2}");
-
-            // Remove
-            string text3 = "Hola que tal";
-
-            // Quitamos la palabra "que"
-            string removeText3 = text3.Remove(5, 4);
-            Console.WriteLine(text);
-            Console.WriteLine(removeText3);
+            string cadenaNumeros = numeros.ToString();
+            Console.WriteLine(cadenaNumeros);
         }
     }
 }
