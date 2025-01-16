a43164835aa3ab76901aca2624dd0bbf3f201004
G58 <freddy.fazbear.troll@gmail.com>
2025-01-16 02:18:23 -0600

    Las secuencias de escape

--- 08LasCadenas/Program.cs
+++ 08LasCadenas/Program.cs
@@ -7,15 +7,38 @@ namespace _08LasCadenas
     {
         static void Main(string[] args)
         {
-            string cadena1 = "Hola a todos";
+            // Secuencias de escape de carácteres
 
-            Console.WriteLine(cadena1);
+            // Secuencia de escape "Nueva línea" /n (Hace un salto de línea)
+            Console.WriteLine("[\\n]: Hola\n mundo!");
 
-            // Después de PadLeft
-            Console.WriteLine(cadena1.PadLeft(15));
+            // Secuencia de escape "Tabulación" /t (Genera una tabulación)
+            Console.WriteLine("[\\t]: Hola\t mundo!");
 
-            // Después de PadRight
-            Console.WriteLine(cadena1.PadRight(15, '#'));
+            // Secuencia de escape "Null" /0 (No es un espacio, es nada)
+            Console.WriteLine("[\\0]: Ho\0la mundo!");
+
+            // Secuencia de escape "Alerta" /a (Genera un sonido de alerta)
+            Console.Write("Presiona cualquier tecla para confirmar: ");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("[\\a]: \aHola mundo!");
+
+            // Secuencia de escape "Retorno de carro" /r (Regresa el cursor al inicio y esvribe lo que faltaba)
+            Console.WriteLine("[\\r]:");
+            Console.WriteLine("Hola mun\rdo!");
+
+            // Secuencia de escape "Retroceso" /b (Regresar el cursos 1 lugar)
+            Console.WriteLine("[\\b]: Hola mun\bdo!");
+
+            // Secuencia de escape "Comilla doble" \" (Permite colocar comillas simples (""))
+            Console.WriteLine("[\\\"]: \"Hola mundo!\"");
+
+            // Secuencia de escape "Comilla simple" \' (Permite colocar comillas simples (''))
+            Console.WriteLine("[\\\']: \'Hola mundo!\'");
+
+            // Secuencia de escape "Barra diagonal invertida" (Permite colocar barra diganol invertida (\))
+            Console.WriteLine("[\\\\]: \\Hola mundo!");
         }
     }
 }
