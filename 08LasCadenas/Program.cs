9931693b97afdc61c2ed98a813540cfe0e811bfb
G58 <freddy.fazbear.troll@gmail.com>
2025-01-16 23:48:25 -0600

    Estructura de una cadena interpolada

--- 08LasCadenas/Program.cs
+++ 08LasCadenas/Program.cs
@@ -7,14 +7,19 @@ namespace _08LasCadenas
     {
         static void Main(string[] args)
         {
-            // CADENAS DE FORMATO
+            //// CADENAS DE FORMATO
 
-            // Interpolación de cadenas
-            // Carácter de cadenas interpolada $
-            int numero1 = 5, numero2 = 10;
+            //// Interpolación de cadenas
+            //// Carácter de cadenas interpolada $
+            //int numero1 = 5, numero2 = 10;
 
-            // Una "literal de cadena entre comillas", es la típica cadena encerrada entre comillas dobles.
-            Console.WriteLine($"El primer valor es: {numero1}, y el segundo valor es: {numero2}");
+            //// Una "literal de cadena entre comillas", es la típica cadena encerrada entre comillas dobles.
+            //Console.WriteLine($"El primer valor es: {numero1}, y el segundo valor es: {numero2}");
+
+            //{expresiónDeInterpolación, alineación: cadenaDeFormato}
+            string nombre = "Luis";
+            int edad = 33;
+            Console.WriteLine($"Hola, soy {nombre,10} y tengo {edad} años");
         }
     }
 }
