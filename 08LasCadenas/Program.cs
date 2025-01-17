0ba9307f111968025869245ec3f27f72a202c161
G58 <freddy.fazbear.troll@gmail.com>
2025-01-16 23:22:16 -0600

    Las cadenas de formato: los formatos compuestos

--- 08LasCadenas/Program.cs
+++ 08LasCadenas/Program.cs
@@ -7,38 +7,17 @@ namespace _08LasCadenas
     {
         static void Main(string[] args)
         {
-            // Secuencias de escape de carácteres
+            // CADENAS DE FORMATO
 
-            // Secuencia de escape "Nueva línea" /n (Hace un salto de línea)
-            Console.WriteLine("[\\n]: Hola\n mundo!");
+            // Formato compuesto
+            int numero1 = 5, numero2 = 10;
 
-            // Secuencia de escape "Tabulación" /t (Genera una tabulación)
-            Console.WriteLine("[\\t]: Hola\t mundo!");
+            Console.WriteLine("El primer valor es: {0}, y el segundo valos es: {1}", numero1, numero2);
 
-            // Secuencia de escape "Null" /0 (No es un espacio, es nada)
-            Console.WriteLine("[\\0]: Ho\0la mundo!");
+            string cadena1;
 
-            // Secuencia de escape "Alerta" /a (Genera un sonido de alerta)
-            Console.Write("Presiona cualquier tecla para confirmar: ");
-            Console.ReadKey();
-            Console.WriteLine();
-            Console.WriteLine("[\\a]: \aHola mundo!");
-
-            // Secuencia de escape "Retorno de carro" /r (Regresa el cursor al inicio y esvribe lo que faltaba)
-            Console.WriteLine("[\\r]:");
-            Console.WriteLine("Hola mun\rdo!");
-
-            // Secuencia de escape "Retroceso" /b (Regresar el cursos 1 lugar)
-            Console.WriteLine("[\\b]: Hola mun\bdo!");
-
-            // Secuencia de escape "Comilla doble" \" (Permite colocar comillas simples (""))
-            Console.WriteLine("[\\\"]: \"Hola mundo!\"");
-
-            // Secuencia de escape "Comilla simple" \' (Permite colocar comillas simples (''))
-            Console.WriteLine("[\\\']: \'Hola mundo!\'");
-
-            // Secuencia de escape "Barra diagonal invertida" (Permite colocar barra diganol invertida (\))
-            Console.WriteLine("[\\\\]: \\Hola mundo!");
+            cadena1 = String.Format("El valor es: {0}", numero1);
+            Console.WriteLine(cadena1);
         }
     }
 }
