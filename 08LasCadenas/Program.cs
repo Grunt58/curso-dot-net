35d9dce531fbd3d1ace0e216796995b0b1f60c1c
G58 <freddy.fazbear.troll@gmail.com>
2025-01-17 02:05:53 -0600

    Convertir cadenas en objetos DateTime Método Parse

--- 08LasCadenas/Program.cs
+++ 08LasCadenas/Program.cs
@@ -7,24 +7,19 @@ namespace _08LasCadenas
     {
         static void Main(string[] args)
         {
-            DateTime fechaHora = new DateTime(2002, 1, 5, 13, 54, 10);
-            Console.WriteLine($"El valor del objketo DateTime es: {fechaHora}");
+            // (es-MX)
+            string cadenaFechaHora;
+            DateTime dateTimeFechaHora;
 
-            // Convirtiendo el objeto DateTime en una cadena
-            // Especificadores de formato personalizado: ("d", "f", "F", "g", "h", "H", "K", "m", "M", "s", "t", "y", "z", ":" o "/")
-            string cadenaFechaHora = fechaHora.ToString("ddd dd MMMM yyyy h:m:s tt");
+            // Pedimos al usuario la fecha/hora y la guardamos en una cadena
+            Console.Write("Ingrese la hora y fecha deseada: ");
+            cadenaFechaHora = Console.ReadLine();
 
-            // Imprimiendo la representación en cadena del objeto DateTime según la referencia cultural actual (es-MX)
-            Console.WriteLine($"La representación en cadena (ddd dd MMMM yyyy h m tt) es: {cadenaFechaHora}");
+            //Convertimos la cadena como un objeto DateTime
+            dateTimeFechaHora = DateTime.Parse(cadenaFechaHora);
 
-            cadenaFechaHora = fechaHora.ToString("ddd dd/MM/yy h:m:s tt");
-            Console.WriteLine($"La representación en cadena (ddd dd/MM/yy h m tt) es: {cadenaFechaHora}");
-
-            cadenaFechaHora = fechaHora.ToString("ddd dd \\de MMMM \\de yyyy h:m:s tt");
-            Console.WriteLine($"La representación en cadena (ddd dd \\de MMMM \\de yyyy h:m:s tt) es: {cadenaFechaHora}");
-
-            cadenaFechaHora = fechaHora.ToString("ddd dd \"de\" MMMM \"de\" yyyy h:m:s tt");
-            Console.WriteLine($"La representación en cadena (ddd dd \"de\" MMMM \"de\" yyyy h:m:s tt) es: {cadenaFechaHora}");
+            // Mostramos a la cadena como un objeto DateTime
+            Console.WriteLine(dateTimeFechaHora);
         }
     }
 }
