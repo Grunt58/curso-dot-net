a6b582b62405c5c0c7866fc4a97111492aab2876
G58 <freddy.fazbear.troll@gmail.com>
2025-01-20 01:49:41 -0600

    Cierre de sección

--- 08LasCadenas/Program.cs
+++ 08LasCadenas/Program.cs
@@ -8,22 +8,7 @@ namespace _08LasCadenas
     {
         static void Main(string[] args)
         {
-            // Hacer un programa que le pida al ususario la fecha de su nacimiento, y nosotros le diremos que día de la semana era.
-
-            // Variables necesarias
-
-            string cadenaFechaNacimiento;
-            DateTime dateTimeFechaNacimiento;
-
-            // Pedimos la fecha de nacimiento
-            Console.Write("Dime la decha de tu nacimiento (día, mes y año) y te diré que día de la semana era: ");
-            cadenaFechaNacimiento = Console.ReadLine();
-
-            // Convertimos la cadena en un objeto DateTime
-            dateTimeFechaNacimiento = DateTime.Parse(cadenaFechaNacimiento);
-
-            // Aplicamos formato de fecha personalizado ("dddd") para mostrar el día de la semana
-            Console.WriteLine($"Naciste un {dateTimeFechaNacimiento.ToString("dddd")}");
+            
         }
     }
 }
