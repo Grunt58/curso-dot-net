3f46e133e136806e7007e7e56a266142ddea9f31
G58 <freddy.fazbear.troll@gmail.com>
2025-01-23 23:48:10 -0600

    Elegir entre clase y estructura: caso 2

--- 10EstructurasYEnumeraciones/Program.cs
+++ 10EstructurasYEnumeraciones/Program.cs
@@ -4,35 +4,23 @@
     {
         static void Main(string[] args)
         {
-            // Conjunto de datos pequeños y específicos
-            // Datos formen parte de un objeto aún más grandes
-
-            Transferencia transferencia1 = new Transferencia();
-            transferencia1.fecha = DateTime.Now;
-            transferencia1.monto = 1000;
-            transferencia1.destinatario = "José Enrique";
-            transferencia1.numeroCuenta = "3498309888";
-            transferencia1.concepto = "Pago del préstamo";
-
-            Console.WriteLine(transferencia1.ToString());
+            
         }
     }
 
-    struct Transferencia
+    class Jugador
     {
-        public DateTime fecha;
-        public double monto;
-        public string destinatario;
-        public string numeroCuenta;
-        public string concepto;
+        string nombre;
+        int vida;
+        int puntaje;
+        List<Item> inventario;
+    }
 
-        public override string ToString()
-        {
-            string mensaje;
-
-            mensaje = $"Fecha: {fecha.ToString()} \nMonto: ${monto} \nDestinatario: {destinatario} \nNúmero de cuenta: {numeroCuenta} \nConcepto: {concepto}";
-
-            return mensaje;
-        }
+    struct Item
+    {
+        string nombre;
+        int tipo;
+        int daño;
+        int vida;
     }
 }
