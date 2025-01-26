e59e618c1077f8533f9af53a914bf75ecec9549d
G58 <freddy.fazbear.troll@gmail.com>
2025-01-26 01:23:54 -0600

    Estructuras anidadas parte 3 (accediendo a los campos)

--- 10EstructurasYEnumeraciones/Program.cs
+++ 10EstructurasYEnumeraciones/Program.cs
@@ -9,6 +9,19 @@
 
             // Nombramos la lista
             listaReproduccion1.Nombre = "Clasicos";
+
+            // Agregamos información a las canciones contenidas en la Lista de Reproducción
+            listaReproduccion1.cancion1.Titulo = "Bohemian Rhapsody";
+            listaReproduccion1.cancion1.Artista = "Queen";
+            listaReproduccion1.cancion1.Duracion = 355;
+
+            listaReproduccion1.cancion2.Titulo = "Viva las Vegas";
+            listaReproduccion1.cancion2.Artista = "Elvis Presley";
+            listaReproduccion1.cancion2.Duracion = 141;
+
+            // Mostramos la información de la lista
+            Console.WriteLine(listaReproduccion1.ToString());
+
         }
     }
 
@@ -23,6 +36,13 @@
         public string Titulo { get => titulo; set => titulo = value; }
         public string Artista { get => artista; set => artista = value; }
         public int Duracion { get => duracion; set => duracion = value; }
+
+        // Invalidamos el método ToString para mostrar la información de la struct
+        public override string ToString()
+        {
+            string mensaje = $"Título: {Titulo}\nArtista: {Artista}\nDuración: {Duracion} segundos";
+            return mensaje;
+        }
     }
 
     struct ListaReproduccion
@@ -36,5 +56,11 @@
         // Anidamos a la struct "Cancion"
         public Cancion cancion1;
         public Cancion cancion2;
+
+        public override string ToString()
+        {
+            string mensaje = $"Lista de reproducción: {Nombre}\n\nCanción 1:\n{cancion1.ToString()}\nCanción 2:\n{cancion2.ToString()}";
+            return mensaje;
+        }
     }
 }
