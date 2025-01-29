dc9f5d73171cf49cb8dc2e6be1701800070a73c3
G58 <freddy.fazbear.troll@gmail.com>
2025-01-28 22:09:58 -0600

    Matrices de estructuras

--- 10EstructurasYEnumeraciones/Program.cs
+++ 10EstructurasYEnumeraciones/Program.cs
@@ -4,63 +4,32 @@
     {
         static void Main(string[] args)
         {
-            // Instanciamos a ListaReproduccion
-            ListaReproduccion listaReproduccion1 = new ListaReproduccion();
+            // Creamos y asignamos instnacias a la matriz en una sola sintaxis
+            Producto[] televisiones = new Producto[]
+            {
+                new Producto { Nombre = "Samsung Neo", Precio = 25000.0, Descripcion = "Televisor OLED de 55 pulgadas" },
+                new Producto { Nombre = "LG Smart TV", Precio = 19000.0, Descripcion = "Televisor Smart de 50 pulgadas" },
+                new Producto { Nombre = "sony Bravia", Precio = 22000.0, Descripcion = "Televisor OLED de 65 pulgadas" }
+            };
 
-            // Nombramos la lista
-            listaReproduccion1.Nombre = "Clasicos";
-
-            // Agregamos información a las canciones contenidas en la Lista de Reproducción
-            listaReproduccion1.cancion1.Titulo = "Bohemian Rhapsody";
-            listaReproduccion1.cancion1.Artista = "Queen";
-            listaReproduccion1.cancion1.Duracion = 355;
-
-            listaReproduccion1.cancion2.Titulo = "Viva las Vegas";
-            listaReproduccion1.cancion2.Artista = "Elvis Presley";
-            listaReproduccion1.cancion2.Duracion = 141;
-
-            // Mostramos la información de la lista
-            Console.WriteLine(listaReproduccion1.ToString());
-
+            // Mostrando la información de los elementos de la matriz
+            foreach (Producto item in televisiones)
+            {
+                // Accede a las propiedades de cada instancia de Producto
+                Console.WriteLine($"Nombre: {item.Nombre}\nPrecio: {item.Precio}\nDescripción: {item.Descripcion}\n");
+            }
         }
     }
 
-    struct Cancion
-    {
-        // Campos
-        string titulo;
-        string artista;
-        int duracion;
-
-        // Propiedades
-        public string Titulo { get => titulo; set => titulo = value; }
-        public string Artista { get => artista; set => artista = value; }
-        public int Duracion { get => duracion; set => duracion = value; }
-
-        // Invalidamos el método ToString para mostrar la información de la struct
-        public override string ToString()
-        {
-            string mensaje = $"Título: {Titulo}\nArtista: {Artista}\nDuración: {Duracion} segundos";
-            return mensaje;
-        }
-    }
-
-    struct ListaReproduccion
+    struct Producto
     {
         // Campos
         string nombre;
+        double precio;
+        string descripcion;
 
-        // Propiedades
         public string Nombre { get => nombre; set => nombre = value; }
-
-        // Anidamos a la struct "Cancion"
-        public Cancion cancion1;
-        public Cancion cancion2;
-
-        public override string ToString()
-        {
-            string mensaje = $"Lista de reproducción: {Nombre}\n\nCanción 1:\n{cancion1.ToString()}\nCanción 2:\n{cancion2.ToString()}";
-            return mensaje;
-        }
+        public double Precio { get => precio; set => precio = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
     }
 }
