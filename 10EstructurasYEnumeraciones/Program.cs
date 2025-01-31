93b2238acd1f5556ffbd480414a5b480d25cb006
G58 <freddy.fazbear.troll@gmail.com>
2025-01-30 22:23:39 -0600

    Los métodos GetProperties y GetMethods

--- 10EstructurasYEnumeraciones/Program.cs
+++ 10EstructurasYEnumeraciones/Program.cs
@@ -33,6 +33,37 @@ namespace _10EstructurasYEnumeraciones
             {
                 Console.WriteLine(item);
             }
+
+            // Mostrando información de la clase "Biblioteca"
+            Console.WriteLine("\n--------------------------\n");
+            Console.WriteLine($"El nombre completo del tipo es: {tipoDatoClass.FullName}");
+
+            // Matrices donde se guardara la información devuelta por los métodos "GetProperties" y "GetMethods"
+            MethodInfo[] metodosDatoClass; // Para los métodos
+            PropertyInfo[] propiedadesDatoClass; // Para las propiedades
+
+            // Usamos a "GetProperties" para obtener la infomancíón de las propiedades de nuestra clase, y almacenamos su devolución en la matriz "propiedadesDatoClass"
+            propiedadesDatoClass = tipoDatoClass.GetProperties();
+
+            // Usamos a "GetMethods" para obtener la infomancíón de las métodos de nuestra clase, y almacenamos su devolución en la matriz "metodosDatoClass"
+            metodosDatoClass = tipoDatoClass.GetMethods();
+
+            // Recorriendo a la matriz "propiedadesDatoClass" para mostrar a las propiedades de "Biblioteca"
+            Console.WriteLine("\nPropiedades del tipo:\n");
+            foreach (PropertyInfo item in propiedadesDatoClass)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            // Recorriendo a la matriz "metodosDatoClass" para mostrar a los métodos de "Biblioteca"
+            Console.WriteLine("Métodos del tipo:\n");
+            foreach (MethodInfo item in metodosDatoClass)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 
@@ -45,28 +76,34 @@ namespace _10EstructurasYEnumeraciones
         bool libroEncontrado;
         int posicionLibroEliminar;
 
+        public Libro[] Libros { get => libros; set => libros = value; }
+        public int CantidadLibros { get => cantidadLibros; set => cantidadLibros = value; }
+        public string BuscarLibro1 { get => buscarLibro; set => buscarLibro = value; }
+        public bool LibroEncontrado { get => libroEncontrado; set => libroEncontrado = value; }
+        public int PosicionLibroEliminar { get => posicionLibroEliminar; set => posicionLibroEliminar = value; }
+
         // Constructor
         public Biblioteca()
         {
-            libros = new Libro[1000]; //Inicializamos la matriz con una capacidad de mil elementos
+            Libros = new Libro[1000]; //Inicializamos la matriz con una capacidad de mil elementos
         }
 
         // Métodos para interactuar con los libros
         public void AgregarLibro()
         {
-            if (cantidadLibros < libros.Length) // Verificar si podemos ingresar un nuevo libro
+            if (CantidadLibros < Libros.Length) // Verificar si podemos ingresar un nuevo libro
             {
                 Console.Clear();
-                Console.WriteLine($"Ingresar información para el libro {cantidadLibros + 1}\n"); // Decimos +1 para mostrar un índice que el usuario entienda
+                Console.WriteLine($"Ingresar información para el libro {CantidadLibros + 1}\n"); // Decimos +1 para mostrar un índice que el usuario entienda
 
                 Console.Write("Ingresa el nombre del libro: ");
-                libros[cantidadLibros].Titulo = Console.ReadLine();
+                Libros[CantidadLibros].Titulo = Console.ReadLine();
                 Console.Write("Ingresa el Autor: ");
-                libros[cantidadLibros].Autor = Console.ReadLine();
+                Libros[CantidadLibros].Autor = Console.ReadLine();
                 Console.Write("Ingresa el año: ");
-                libros[cantidadLibros].Año = Console.ReadLine();
+                Libros[CantidadLibros].Año = Console.ReadLine();
 
-                cantidadLibros++; // Aumentamos la cantidad de libros existentes
+                CantidadLibros++; // Aumentamos la cantidad de libros existentes
 
                 Console.Clear();
                 Console.WriteLine("¡Libro agregado correctamente!\n");
@@ -82,16 +119,16 @@ namespace _10EstructurasYEnumeraciones
         {
             Console.Clear();
 
-            if (cantidadLibros == 0)
+            if (CantidadLibros == 0)
             {
                 // Si no existe ningún libro, mostramos el sig. mensaje
                 Console.WriteLine("¡Biblioteca vacía! Agrega libros para poder visualizarlos");
             }
             else
             {
-                for (int i = 0; i < cantidadLibros; i++) // Si existe al menos uno, mostramos   su información
+                for (int i = 0; i < CantidadLibros; i++) // Si existe al menos uno, mostramos   su información
                 {
-                    Console.WriteLine($"{i + 1}.- Título = {libros[i].Titulo}, Autor = {libros[i].Autor}, Año = {libros[i].Año}");
+                    Console.WriteLine($"{i + 1}.- Título = {Libros[i].Titulo}, Autor = {Libros[i].Autor}, Año = {Libros[i].Año}");
                 }
 
                 Console.Write("\nPresiona cualquier tecla para continuar... ");
@@ -105,18 +142,18 @@ namespace _10EstructurasYEnumeraciones
             Console.Clear();
 
             Console.Write("Ingresa el nombre exacto del libro para buscarlo: ");
-            buscarLibro = Console.ReadLine();
-            libroEncontrado = false;//Inicializamos al campo en "false" para indicar que al iniciar el recorrido por la matriz no hemos encontrado un libro
+            BuscarLibro1 = Console.ReadLine();
+            LibroEncontrado = false;//Inicializamos al campo en "false" para indicar que al iniciar el recorrido por la matriz no hemos encontrado un libro
 
-            for (int i = 0; i < cantidadLibros; i++)
+            for (int i = 0; i < CantidadLibros; i++)
             {
-                if (libros[i].Titulo.Equals(buscarLibro))
+                if (Libros[i].Titulo.Equals(BuscarLibro1))
                 {
-                    Console.WriteLine($"El libro \"{libros[i].Titulo}\" del autor(a): \"{libros[i].Autor}\" se encuentra disponible en la biblioteca en el índice: {i + 1}");
-                    libroEncontrado = true; // Asignamos "true" para indicar que hemos encontrado el libro, y así evitar entrar en el siguiente "if"
+                    Console.WriteLine($"El libro \"{Libros[i].Titulo}\" del autor(a): \"{Libros[i].Autor}\" se encuentra disponible en la biblioteca en el índice: {i + 1}");
+                    LibroEncontrado = true; // Asignamos "true" para indicar que hemos encontrado el libro, y así evitar entrar en el siguiente "if"
                 }
             }
-            if (!libroEncontrado) //Lo mismo que colocar if(libroEncontrado == false)
+            if (!LibroEncontrado) //Lo mismo que colocar if(libroEncontrado == false)
             {
                 Console.WriteLine("¡Libro no encontrado!\n");
             }
@@ -127,19 +164,19 @@ namespace _10EstructurasYEnumeraciones
             Console.Clear();
 
             Console.Write("Ingresa al menos una parte del título o del nombre del autor de un libro para buscarlo: ");
-            buscarLibro = Console.ReadLine().ToLower();
+            BuscarLibro1 = Console.ReadLine().ToLower();
 
-            libroEncontrado = false;//Inicializamos la variable en "false" para indicar que al iniciar el recorrido por la matriz no hemos encontrado un libro
+            LibroEncontrado = false;//Inicializamos la variable en "false" para indicar que al iniciar el recorrido por la matriz no hemos encontrado un libro
 
-            for (int i = 0; i < cantidadLibros; i++)
+            for (int i = 0; i < CantidadLibros; i++)
             {
-                if (libros[i].Titulo.ToLower().Contains(buscarLibro) || libros[i].Autor.ToLower().Contains(buscarLibro))
+                if (Libros[i].Titulo.ToLower().Contains(BuscarLibro1) || Libros[i].Autor.ToLower().Contains(BuscarLibro1))
                 {
-                    Console.WriteLine($"La palabra \"{buscarLibro}\" fue encontrada en el libro: \"{libros[i].Titulo}\" del autor(a): \"{libros[i].Autor}\" en el índice: {i + 1}");
-                    libroEncontrado = true; // Asignamos "true" para indicar que hemos encontrado el libro, y así evitar entrar en el siguiente "if"
+                    Console.WriteLine($"La palabra \"{BuscarLibro1}\" fue encontrada en el libro: \"{Libros[i].Titulo}\" del autor(a): \"{Libros[i].Autor}\" en el índice: {i + 1}");
+                    LibroEncontrado = true; // Asignamos "true" para indicar que hemos encontrado el libro, y así evitar entrar en el siguiente "if"
                 }
             }
-            if (!libroEncontrado) //Lo mismo que colocar if(encontrar == false)
+            if (!LibroEncontrado) //Lo mismo que colocar if(encontrar == false)
             {
                 Console.WriteLine("¡No se encontraron coincidencias!\n");
             }
@@ -150,33 +187,33 @@ namespace _10EstructurasYEnumeraciones
         {
             Console.Clear();
 
-            if (cantidadLibros == 0)
+            if (CantidadLibros == 0)
             {
                 Console.WriteLine("¡La biblioteca está vacía, no hay nada que eliminar!");
             }
             else
             {
-                Console.Write($"Ingrese el número de libro que desea eliminar (Del 1 al {cantidadLibros}): ");
-                posicionLibroEliminar = Convert.ToInt32(Console.ReadLine()) - 1; //Decimos que es -1 para que el índice ingresado por el usuario coincida con el índice real de matriz
+                Console.Write($"Ingrese el número de libro que desea eliminar (Del 1 al {CantidadLibros}): ");
+                PosicionLibroEliminar = Convert.ToInt32(Console.ReadLine()) - 1; //Decimos que es -1 para que el índice ingresado por el usuario coincida con el índice real de matriz
 
                 // Verificamos que el número ingresado sea válido
-                if (posicionLibroEliminar >= 0 && posicionLibroEliminar < cantidadLibros)
+                if (PosicionLibroEliminar >= 0 && PosicionLibroEliminar < CantidadLibros)
                 {
                     //Confirmamos si el libro que ingresó es el que quiere eliminar
-                    Console.Write($"¿El libro que deseas eliminar es: \"{libros[posicionLibroEliminar].Titulo}\"? (Sí/No): ");
+                    Console.Write($"¿El libro que deseas eliminar es: \"{Libros[PosicionLibroEliminar].Titulo}\"? (Sí/No): ");
                     string opcion = Console.ReadLine().ToLower();
 
                     if (opcion == "si")
                     {
                         // Variables para mostrar un mensaje de cuál fue el libro eliminado
-                        string tituloEliminado = libros[posicionLibroEliminar].Titulo;
-                        string autorEliminado = libros[posicionLibroEliminar].Autor;
+                        string tituloEliminado = Libros[PosicionLibroEliminar].Titulo;
+                        string autorEliminado = Libros[PosicionLibroEliminar].Autor;
 
-                        for (int i = posicionLibroEliminar; i < cantidadLibros; i++)
+                        for (int i = PosicionLibroEliminar; i < CantidadLibros; i++)
                         {
-                            libros[i] = libros[i + 1];
+                            Libros[i] = Libros[i + 1];
                         }
-                        cantidadLibros--; //Reducimos la cantidad de libros en uno, por el que acabamos de eliminar
+                        CantidadLibros--; //Reducimos la cantidad de libros en uno, por el que acabamos de eliminar
 
                         // Le mostramos al usuario el libro que se eliminó
                         Console.WriteLine($"\n¡El libro \"{tituloEliminado}\" del autor(a): \"{autorEliminado}\" fue eliminado!");
