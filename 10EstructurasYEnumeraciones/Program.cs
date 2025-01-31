5921c15f3d57a1c442f46b7ee9900afa98f2ff40
G58 <freddy.fazbear.troll@gmail.com>
2025-01-30 20:43:27 -0600

    Las enumeraciones y su tipo subyacente

--- 10EstructurasYEnumeraciones/Program.cs
+++ 10EstructurasYEnumeraciones/Program.cs
@@ -6,27 +6,14 @@ namespace _10EstructurasYEnumeraciones
     {
         static void Main(string[] args)
         {
-            // Declaramos una variable del tipo del enum
-            // Variables enumeradas, variable de enumeración, variables de tipo enumerado
-            Semana diaDePago = Semana.Viernes;
-
-            // Ejemplo de uso en una estructura de control
-            if (diaDePago == Semana.Viernes)
-            {
-                double sueldoSemana1 = 3500;
-                Console.WriteLine($"Hoy es dia de pago, recibes: ${sueldoSemana1}");
-            }
-            else
-            {
-                Console.WriteLine("Lo sentimos, hoy no es día de pago.");
-            }
+            
         }
     }
 
-    // Declaración de una "enum"
-    enum Semana
+    enum BonosSueldosJefes : long 
     {
-        // Conjunto de constantes con nombre ("enumeradores" o "miembros de enumeración")
-        Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo
+        PrimerNivel = 6_000_000_000,
+        SegundoNivel = 4_000_000_000 ,
+        TercerNivel = 2_000_000_000 // 2 mil millones ($2,000,000,000)
     }
 }
