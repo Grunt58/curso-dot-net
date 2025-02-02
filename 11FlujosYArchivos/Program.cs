44662e350fb5d37fbb07c2659ea867e9075f37df
G58 <freddy.fazbear.troll@gmail.com>
2025-02-01 23:07:46 -0600

    Los stream en la memoria y la clase MemoryStream

--- 11FlujosYArchivos/Program.cs
+++ 11FlujosYArchivos/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace _11FlujosYArchivos
+﻿using System.IO;
+
+namespace _11FlujosYArchivos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creamos un flujo/stream para trabajar con la memoria RAM
+            MemoryStream ms1 = new MemoryStream();
         }
     }
 }
