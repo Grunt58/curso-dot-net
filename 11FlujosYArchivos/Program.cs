b0ccff11afd9eb58a62fcfc2e89ea3b4fbc7527d
G58 <freddy.fazbear.troll@gmail.com>
2025-02-01 23:06:20 -0600

    Sección 11: Flujos y Archivos

---
+++ 11FlujosYArchivos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11FlujosYArchivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
