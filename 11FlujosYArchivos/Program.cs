309f1f43ab0a1631ec2c125e692e4d37d8fc98e1
G58 <freddy.fazbear.troll@gmail.com>
2025-02-02 00:05:00 -0600

    Escribir datos en un stream usando el método 'Write'

--- 11FlujosYArchivos/Program.cs
+++ 11FlujosYArchivos/Program.cs
@@ -10,6 +10,9 @@ namespace _11FlujosYArchivos
             // Creamos un flujo/stream para trabajar con la memoria RAM
             MemoryStream ms1 = new MemoryStream();
 
+            // Mostrando la capacidad, longitud y posición del puntero del stream
+            InformacionStream(ms1);
+
             // Le pedimos al ususario una cadena para guardar en el stream
             Console.Write("Ingresa un texto para guardarlo en el stream: ");
             string cadena1 = Console.ReadLine();
@@ -21,6 +24,19 @@ namespace _11FlujosYArchivos
 
             // Codificamos a cadena1 para obtener una secuenda de bytes
             matrizCadenaByte = Encoding.UTF8.GetBytes(cadena1);
+
+            // Escribiendo datos en el flujo
+            ms1.Write(matrizCadenaByte, 0, matrizCadenaByte.Length);
+
+            // Mostrando la capacidad, longitud y posición del puntero del stream
+            InformacionStream(ms1);
+        }
+
+        static void InformacionStream(MemoryStream ms1Pa)
+        {
+            Console.WriteLine($"\nCapacidad: {ms1Pa.Capacity}");
+            Console.WriteLine($"Longitud: {ms1Pa.Length}");
+            Console.WriteLine($"Posición del puntero: {ms1Pa.Position}\n");
         }
     }
 }
