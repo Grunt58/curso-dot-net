b4a3830f22f62dec9b241c9a57073d5093d91b86
G58 <freddy.fazbear.troll@gmail.com>
2025-02-01 23:16:09 -0600

    La clase Encoding y cómo codificar un string

--- 11FlujosYArchivos/Program.cs
+++ 11FlujosYArchivos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 
 namespace _11FlujosYArchivos
 {
@@ -8,6 +9,18 @@ namespace _11FlujosYArchivos
         {
             // Creamos un flujo/stream para trabajar con la memoria RAM
             MemoryStream ms1 = new MemoryStream();
+
+            // Le pedimos al ususario una cadena para guardar en el stream
+            Console.Write("Ingresa un texto para guardarlo en el stream: ");
+            string cadena1 = Console.ReadLine();
+
+            byte[] matrizCadenaByte;
+
+            // Codificar: string -> secuencia de bytes
+            // Descodificar: secuencia de bytes -> string
+
+            // Codificamos a cadena1 para obtener una secuenda de bytes
+            matrizCadenaByte = Encoding.UTF8.GetBytes(cadena1);
         }
     }
 }
