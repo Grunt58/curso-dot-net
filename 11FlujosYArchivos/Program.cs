54dc8a98754cae7b0a1f34e12980cc298bbd00c7
G58 <freddy.fazbear.troll@gmail.com>
2025-02-02 18:52:10 -0600

    Ejercicio

--- 11FlujosYArchivos/Program.cs
+++ 11FlujosYArchivos/Program.cs
@@ -7,53 +7,151 @@ namespace _11FlujosYArchivos
     {
         static void Main(string[] args)
         {
-            // Creamos un flujo/stream para trabajar con la memoria RAM
-            MemoryStream ms1 = new MemoryStream();
+            // Variables necesarias
+            int opcion;
+            bool repetir = true;
+            string mensajeParaCifrar, contraseñaMensaje, mensajeCifrado;
 
-            // Mostrando la capacidad, longitud y posición del puntero del stream
-            InformacionStream(ms1);
+            // Creamos un flujo/stream en la memoria RAM
+            MemoryStream memoryStream1 = new MemoryStream();
 
-            // Matriz de bytes para guardar a la cadena convertida
-            byte[] matrizCadenaByte;
+            // Pedimos la cadena que va a ser guardada en el flujo
+            Console.Write("Ingresa el mensaje que quieres cifrar: ");
+            mensajeParaCifrar = Console.ReadLine();
 
+            // Pedimos una contraseña para porteger el mensaje cifrado
+            Console.Write("Ingresa una contraseña para proteger el mensaje: ");
+            contraseñaMensaje = Console.ReadLine();
 
-            // Codificamos a cadena1 para obtener una secuenda de bytes
-            matrizCadenaByte = Encoding.UTF8.GetBytes("Hola a todos");
+            // Enviamos la cadena dada por el ususario para cifrarla
+            mensajeCifrado = CifrarMensaje(mensajeParaCifrar);
 
-            // Escribiendo datos en el flujo
-            ms1.Write(matrizCadenaByte, 0, matrizCadenaByte.Length);
+            // Declaramos una matriz de bytes y le asignamos la codificación del mensaje ya cifrado para obtener una secuencia de bytes
+            byte[] matrizCadenaByte = Encoding.UTF8.GetBytes(mensajeCifrado);
 
-            // Mensaje para separar el antes y despupes del write
-            Console.WriteLine("Después de escribir en el stream...");
-            // Mostrando la capacidad, longitud y posición del puntero del stream
-            InformacionStream(ms1);
+            // Escribimos el mensaje cifrado en el flujo
+            memoryStream1.Write(matrizCadenaByte, 0, matrizCadenaByte.Length);
+
+            // Mensaje para separa el antes y después del write
+            Console.WriteLine("El mensaje está protegido, presiona cualquier tecla para continuar...");
+            Console.ReadKey();
+
+            // Empezamos a leer el string (cifrado) alacenado en el memorystream
 
             // Búfer para almacenar los bytes leídos por Read
             byte[] buferBytesLeidos = new byte[100];
 
-            ms1.Seek(0, SeekOrigin.Begin); // Mover el puntero al inicio del flujo
+            // Mover el puntero al inicio del flujo 
+            memoryStream1.Seek(0, SeekOrigin.Begin);
 
             // Leemos el contenido de nuestro flujo usando el método Read
-            int bytesLeidos = ms1.Read(buferBytesLeidos, 0, (int)ms1.Length);
+            memoryStream1.Read(buferBytesLeidos, 0, (int)memoryStream1.Length);
 
-            // Mostramos cuántos bytes fueron leídos por Read
-            Console.WriteLine($"Bytes leídos del stream: {bytesLeidos}");
+            // Descodificamos la matriz de bytes leída para convertirla en un string
+            string cadenaDescodificadaCifrada = Encoding.UTF8.GetString(buferBytesLeidos);
 
-            // Descodificamos la matriz de bytes leida para convertirla en un string
-            string cadenaDescodificada = Encoding.UTF8.GetString(buferBytesLeidos);
+            // Mostramos el menú
+            do
+            {
+                // Limpiamos la consola
+                Console.Clear();
 
-            // Mostramos la cadena descodificada
-            Console.WriteLine($"Información obtenida del stream: \"{cadenaDescodificada}\"");
+                Console.WriteLine("[1]: Mostrar mensaje");
+                Console.WriteLine("[2]: Descifrar mensaje");
+                Console.WriteLine("[3]: Me rindo");
 
-            // Cerramos el flujo
-            ms1.Close();
+                Console.Write("Escoje una opción: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        // Mostramos la cadena descodificada pero todavía cifrada
+                        Console.WriteLine($"Mensaje: \"{cadenaDescodificadaCifrada}\"");
+
+                        // Mensaje de espera
+                        Console.WriteLine("Presiona cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                        
+                    case 2:
+                        Console.Write("Ingresa la contraseña para descifrar el mensaje: ");
+                        string posibleContraseña = Console.ReadLine();
+
+                        if (posibleContraseña == contraseñaMensaje)
+                        {
+                            // Mandamos la cadena codificada al método "DescifrarMensaje" para ser descifrada, y la devolución la guardamos en una variable local
+                            string mensajeDescifrado = DescifrarMensaje(cadenaDescodificadaCifrada);
+
+                            // Mostramos la cadena descodificada y ya descifrada
+                            Console.WriteLine($"Mensaje: \"{mensajeDescifrado}\"");
+                            // Mensaje de espera
+                            Console.WriteLine("Presiona cualquier tecla para continuar...");
+                            Console.ReadKey();
+
+                            // Cerramos el flujo
+                            memoryStream1.Close();
+
+                            // Finalizamos el programa
+                            repetir = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Constraseña incorrecta. Intenta de nuevo");
+                            // Mensaje de espera
+                            Console.WriteLine("Presiona cualquier tecla para continuar...");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case 3:
+                        // Finalizamos el programa
+                        repetir = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            } while (repetir);
         }
 
-        static void InformacionStream(MemoryStream ms1Pa)
+        static string CifrarMensaje(string mensajeCifrarPa)
         {
-            Console.WriteLine($"\nCapacidad: {ms1Pa.Capacity}");
-            Console.WriteLine($"Longitud: {ms1Pa.Length}");
-            Console.WriteLine($"Posición del puntero: {ms1Pa.Position}\n");
+            // Variable que va a guardar el mensaje cifrado
+            string mensajeCifrado;
+
+            // Le asignamos el mensaje original a nuestra variable local vacía
+            mensajeCifrado = mensajeCifrarPa;
+
+            // Reemplazamos las vocales por números en nuestro mensaje almacenado en la variable local
+            mensajeCifrado = mensajeCifrado.Replace('a', '1');
+            mensajeCifrado = mensajeCifrado.Replace('b', '2');
+            mensajeCifrado = mensajeCifrado.Replace('c', '3');
+            mensajeCifrado = mensajeCifrado.Replace('d', '4');
+            mensajeCifrado = mensajeCifrado.Replace('e', '5');
+
+            // Devolvemos el mensaje ya cifrado
+            return mensajeCifrado;
+        }
+
+        static string DescifrarMensaje(string mensajeDescifrarPa)
+        {
+
+            // Variable que va a guardar el mensaje cifrado
+            string mensajeDescifrado;
+
+            // Le asignamos el mensaje original a nuestra variable local vacía
+            mensajeDescifrado = mensajeDescifrarPa;
+
+            // Reemplazamos las vocales por números en nuestro mensaje almacenado en la variable local
+            mensajeDescifrado = mensajeDescifrado.Replace('1', 'a');
+            mensajeDescifrado = mensajeDescifrado.Replace('2', 'b');
+            mensajeDescifrado = mensajeDescifrado.Replace('3', 'c');
+            mensajeDescifrado = mensajeDescifrado.Replace('4', 'd');
+            mensajeDescifrado = mensajeDescifrado.Replace('5', 'e');
+
+            // Devolvemos el mensaje ya cifrado
+            return mensajeDescifrado;
         }
     }
 }
