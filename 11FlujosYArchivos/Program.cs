353ae0b1e4ea6116b50e67ffd566a4a9f6d926d0
G58 <freddy.fazbear.troll@gmail.com>
2025-02-02 20:33:18 -0600

    La clase File y el método Create

--- 11FlujosYArchivos/Program.cs
+++ 11FlujosYArchivos/Program.cs
@@ -7,20 +7,11 @@ namespace _11FlujosYArchivos
     {
         static void Main(string[] args)
         {
-            // Creando un flujo de archivo
-            FileStream fs = new FileStream("contraseña.txt", FileMode.Open);
+            // Creamos un objeto FileStream
+            FileStream fs;
 
-            // buffer para almacenar los bytes contenidos en el archivo
-            byte[] bufferLeerDatosArchivo = new byte[1024];
-
-            // Leemos los bytes que contiene el archivo
-            fs.Read(bufferLeerDatosArchivo, 0, bufferLeerDatosArchivo.Length);
-
-            // Descodificamos la matriz de bytes leída para convertirla en un string
-            string cadenaDescodificada = Encoding.UTF8.GetString(bufferLeerDatosArchivo);
-
-            // Mostramos la cadena descodificada (la información del archivo)
-            Console.WriteLine($"Información obtenida del archivo: \"{cadenaDescodificada}\"");
+            // Creamos un archivo
+            fs = File.Create("Archivo con file.txt");
         }
     }
 }
