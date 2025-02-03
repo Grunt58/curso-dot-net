0c620acbb4f40e9ab825f3d33b66e3dd9158ff08
G58 <freddy.fazbear.troll@gmail.com>
2025-02-03 14:17:02 -0600

    Saber si existe un directorio

--- 11FlujosYArchivos/Program.cs
+++ 11FlujosYArchivos/Program.cs
@@ -7,13 +7,14 @@ namespace _11FlujosYArchivos
     {
         static void Main(string[] args)
         {
-            // Creamos nuestro primer directorio
-            DirectoryInfo directoryInfo = Directory.CreateDirectory("Mi cuarta carpeta");
-
-            // Mostramos información del directorio creado, a traves del objeto DirectoryInfo y sus propiedades
-            Console.WriteLine($"Nombre: {directoryInfo.Name}");
-            Console.WriteLine($"Ruta absoluta: {directoryInfo.FullName}");
-            Console.WriteLine($"Fecha de creación: {directoryInfo.CreationTime}");
+            if (Directory.Exists("Mi segunda carpeta"))
+            {
+                Console.WriteLine("Se encontró el directorio");
+            }
+            else
+            {
+                Console.WriteLine("Directorio no encontrado");
+            }
         }
     }
 }
