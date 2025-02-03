3b99ba94774a56d6fb1ae94cb55dcfd19c5bce11
G58 <freddy.fazbear.troll@gmail.com>
2025-02-02 20:14:14 -0600

    Escribir información en un archivo de texto

--- 11FlujosYArchivos/Program.cs
+++ 11FlujosYArchivos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 
 namespace _11FlujosYArchivos
 {
@@ -8,6 +9,22 @@ namespace _11FlujosYArchivos
         {
             // Creando un flujo de archivo
             FileStream fs = new FileStream("mi_primer_archivo.txt", FileMode.Create);
+
+            // Indicando que el archivo se ha creado
+            Console.WriteLine("Archivo creado con exito");
+
+            // Cadena para guardar la información que será escrita en el archivo txt
+            string textoParaArchivo = "Texto de prueba para el archivo";
+
+            // Matriz de bytes para almacenar la cadena
+            byte[] bufferCadenaABytes;
+
+            // Codificamos la cadena
+            bufferCadenaABytes = Encoding.UTF8.GetBytes(textoParaArchivo);
+
+            // Escribimos a la cadena codificada en nuestro archivo de texto
+            fs.Write(bufferCadenaABytes, 0, bufferCadenaABytes.Length);
+            fs.Close();
         }
     }
 }
