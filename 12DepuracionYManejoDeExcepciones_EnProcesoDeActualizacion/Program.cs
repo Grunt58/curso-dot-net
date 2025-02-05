7e0dbf15a01996383b8d766ae04f8f499e1a77e2
G58 <freddy.fazbear.troll@gmail.com>
2025-02-05 14:37:16 -0600

    Sección 12: Depuración y manejo de excepciones (en proceso de actualización)

---
+++ 12DepuracionYManejoDeExcepciones_EnProcesoDeActualizacion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12DepuracionYManejoDeExcepciones_EnProcesoDeActualizacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
