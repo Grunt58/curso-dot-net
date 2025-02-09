c2e9f6e783a6f4b4a207ca126e8e35d67bfae3b5
G58 <freddy.fazbear.troll@gmail.com>
2025-02-09 17:28:46 -0600

    Sección 13: Proyecto final 1: Videojuego Gato

---
+++ 13ProyectoFinal_VideojuegoGato/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13ProyectoFinal_VideojuegoGato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
