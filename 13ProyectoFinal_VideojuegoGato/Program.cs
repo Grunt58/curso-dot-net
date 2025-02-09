1ba598a7dd87405f8eff1e8bc419fefd5f8959bd
G58 <freddy.fazbear.troll@gmail.com>
2025-02-09 17:39:09 -0600

    Creación de arreglos

--- 13ProyectoFinal_VideojuegoGato/Program.cs
+++ 13ProyectoFinal_VideojuegoGato/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        // Creamos un arreglo bidimensional para el tablero del juego
+        static int[,] tablero = new int[3, 3]; // 3 filas - 3 columnas
+
+        // Creamos un arreglo para los símbolos del tablero: Espacio en blanco, jug.1, jug.2
+        static char[] simbolo = { ' ', 'O', 'X' };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
