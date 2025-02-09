335f79bed2d49da315ae9f77533bb2cdbcf1eddf
G58 <freddy.fazbear.troll@gmail.com>
2025-02-08 19:19:50 -0600

    La instrucción de control de excepciones 'try-catch' y la clase 'Exception'

--- 12DepuracionYManejoDeExcepciones_EnProcesoDeActualizacion/Program.cs
+++ 12DepuracionYManejoDeExcepciones_EnProcesoDeActualizacion/Program.cs
@@ -2,9 +2,50 @@
 {
     internal class Program
     {
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int opcion;
+
+            do
+            {
+                // Menú
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+
+                Console.Write("Escoge una opción: ");
+                try
+                {
+                    // Código que puede generar una excepción (se intentará ejecutar)
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    // Código para controlar la excepción de TipoDeExcepcion
+                    Console.WriteLine("¡Solo es posible ingrear el número 1 o 2!");
+                    opcion = 5;
+                }
+
+            } while (opcion < 1 || opcion > 2); 
+
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Sumar");
+                    break;
+                case 2:
+                    Console.WriteLine("Restar");
+                    break;
+                default:
+                    Console.WriteLine("El número ingresado no es válido");
+                    break;
+            }
         }
     }
 }
